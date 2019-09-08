dbe45aab327449b3ad3730c975d614835c2a8b31
N-Smith00 <55036396+N-Smith00@users.noreply.github.com>
2019-09-08 15:23:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,18 +9,17 @@ async def on_ready():
     print('mod bot reporting for duty')
     print(client.user)
     channel = client.get_channel(620095182871855104)
-    print(channel)
     ranks = await channel.send('react with your rank to let everyone else know your skill')
-    # emoji = client.get_emoji(\N{HEART})
-    await ranks.add_reaction('\U0001F493')
-
-@client.event
-async def ranks():
-    #channel = client.get_channel(620095182871855104)
-    #print(channel)
-    #ranks = await client.send(channel, 'react with your rank to let everyone else know your skill')
-    #await client.add_reaction(ranks, ':rose:')
-    # all this doesnt work yet, find a way to put it into it's own function
+    global ranks
+    await ranks.add_reaction(':Iron:620335275192614953')
+    await ranks.add_reaction(':Bronze:620334694826770489')
+    await ranks.add_reaction(':Silver:620334883868508184')
+    await ranks.add_reaction(':Gold:620335348018446341')
+    await ranks.add_reaction(':Platinium:620335395787505684')
+    await ranks.add_reaction(':Diamond:620335430952419372')
+    await ranks.add_reaction(':Master:620335475328286741')
+    await ranks.add_reaction(':Grandmaster:620335512070258718')
+    await ranks.add_reaction(':Challenger:620335550733352980')
 
 keep_alive()
 token = os.environ.get('DISCORD_BOT_SECRET')
